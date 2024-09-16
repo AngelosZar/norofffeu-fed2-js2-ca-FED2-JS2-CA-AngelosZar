00088a1e26755962ac03ee9171e56cb398591af1
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-16 17:13:20 +0200

    Update login.js
    Updated and finalised Log in function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,31 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from '../../api/constants.js';
+
+export async function login({ email, password }) {
+  try {
+    const res = await fetch(API_AUTH_LOGIN, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    if (res.ok) {
+      const responseData = await res.json();
+      const accessToken = responseData.data.accessToken;
+      const currentUser = responseData.data.name;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('name', responseData.data.name);
+      confirm('You are now logged in');
+      return { responseData, accessToken, currentUser };
+    } else {
+      alert('Invalid username or password\nPlease try again');
+      //   cannot reset form here reset on onlogin page
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
