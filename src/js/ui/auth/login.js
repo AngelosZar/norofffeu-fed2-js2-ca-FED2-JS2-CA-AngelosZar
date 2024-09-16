89b989911c936ee907c97d3da8bb087eea78af7d
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-16 17:43:32 +0200

    Created onLogin function/updated dependencies
    on login function is handling the click event on the target - form .It gathers the user input .login function is sending the request .

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,19 @@
-export async function onLogin(event) {}
+import { login } from '../../api/auth/login.js';
+
+export async function onLogin(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.email.value;
+  const password = form.password.value;
+  const data = { email, password };
+  const response = await login(data);
+  if (response) {
+    form.reset();
+    //
+    location.href = '/profile/index.html'; // page cannot be found in scr vies ???
+    //
+  } else {
+    form.reset();
+    throw new Error('Something went wrong\nPlease try again');
+  }
+}
