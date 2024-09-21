b289d9b905f35984e78feab14d5c1d592ee2bd6b
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-21 18:51:00 +0200

    Delete clatter and add error handling on delete function

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -2,9 +2,6 @@ import { API_BASE, API_KEY } from '../constants';
 
 export async function deletePost(id) {
   try {
-    // const apiKey = localStorage.getItem('apiKey');
-    // const accessToken = localStorage.getItem('accessToken');
-
     const response = await fetch(`${API_BASE}/social/posts/${id}`, {
       method: 'DELETE',
       headers: {
@@ -12,13 +9,10 @@ export async function deletePost(id) {
         authorization: `Bearer ${localStorage.getItem('accessToken')}`,
         'X-Noroff-API-Key': `${localStorage.getItem('apiKey')}`,
       },
-      //   authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     });
     if (!response.ok) {
       const data = await response.json();
-      console.log(API_KEY);
-      console.log(data);
-      console.log(accessToken);
+      alert(`Failed to delete post \n${data.errors[0].message}`);
       throw new Error('Failed to delete post');
     }
   } catch (error) {
