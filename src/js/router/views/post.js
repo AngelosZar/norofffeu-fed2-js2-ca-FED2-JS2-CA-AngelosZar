a9581c4d2a82d9c1e1ac1b2c7f9d7b4202541609
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-25 18:15:26 +0200

    Refactoring rendering functions
    I have created a helper function that creates the html content with arguments being the parent container that they are going to be displayed and the response data of the api request

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -34,7 +34,32 @@ export const handleDeletingPost = async function (event) {
     alert(error.message);
   }
 };
+const generateHtml = async function (parentDiv, responseData) {
+  const parentContainer = document.querySelector(`#${parentDiv}`);
+  responseData.forEach(post => {
+    const html = `
+        <div class="card-for-posts">
+          <h3 class="title-for-post">${post.title}</h3>
+          <p class="body-for-post">${post?.body}</p>
+          <p class="tags-for-post">${post?.tags.join(' / ')}</p>
+          <img class="media-for-post" src="${post?.media?.url ?? ''}" alt="${
+      post?.media?.alt ?? ''
+    }" />
 
+           <a href="../post/edit/" class="btn-action1" data-post-id="${
+             post.id
+           }" >Edit Post</a>
+          <a href="#" id="deleteCurrentPost" class="btn-action1">Delete Post</a>
+         </div> `;
+
+    parentContainer.insertAdjacentHTML('beforeend', html);
+    const postId = post.id;
+    // console.log(postId);
+    return postId;
+  });
+  parentContainer.addEventListener('click', handleDeletingPost);
+  parentContainer.addEventListener('click', handleEditPost);
+};
 const renderSinglePost = async function (id) {
   try {
     const post = await readPost(id);
@@ -68,44 +93,20 @@ const renderSinglePost = async function (id) {
   }
 };
 
-// //
-// const renderMultiplePosts = async function (limit, page, tag) {
-//   try {
-//     const responseData = await readPosts(limit, page, tag);
+//
+export const renderMultiplePosts = async function (limit, page, tag) {
+  try {
+    const responseData = await readPosts(limit, page, tag);
 
-//     if (!responseData) {
-//       throw new Error('No data found\nPlease try again later');
-//     }
+    if (!responseData) {
+      throw new Error('No data found\nPlease try again later');
+    }
+    await generateHtml('many_posts', responseData);
+  } catch (error) {
+    alert(error);
+    throw error;
+  }
+};
 
-//     const parentContainer = document.querySelector('#many_posts');
-//     responseData.forEach(post => {
-//       const html = `
-//           <div class="card-for-posts">
-//             <h3 class="title-for-post">${post.title}</h3>
-//             <p class="body-for-post">${post?.body}</p>
-//             <p class="tags-for-post">${post?.tags.join(' / ')}</p>
-//             <img class="media-for-post" src="${post?.media?.url ?? ''}" alt="${
-//         post?.media?.alt ?? ''
-//       }" />
-
-//              <a href="../post/edit/" class="btn-action1" data-post-id="${
-//                post.id
-//              }" >Edit Post</a>
-//             <a href="#" id="deleteCurrentPost" class="btn-action1">Delete Post</a>
-//            </div> `;
-
-//       parentContainer.insertAdjacentHTML('beforeend', html);
-//       const postId = post.id;
-//       // console.log(postId);
-//       return postId;
-//     });
-//     parentContainer.addEventListener('click', handleDeletingPost);
-//     parentContainer.addEventListener('click', handleEditPost);
-//   } catch (error) {
-//     alert(error);
-//     throw error;
-//   }
-// };
-
-// await renderMultiplePosts(12, 1, 'tag');
-// await renderSinglePost(1069);
+await renderMultiplePosts(12, 1, 'tag');
+await renderSinglePost(1069);
