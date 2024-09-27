ba70cf49d174405b469734b11f7b19e86aa6112d
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-27 15:50:27 +0200

    update profile functionality
    Created a simple fetch function that will be later given the users input and send it as the body of the request .

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,1 +1,50 @@
-export async function updateProfile(username, { avatar, banner }) {}
+// create a helper function to get data from user // form
+// return body data
+const body = ` {
+    avatar: {
+      url: '${avatarUrl}',
+      alt: '${avatarAlt}',
+    },
+    banner: {
+      url: '${bannerUrl}',
+      alt: '${bannerAlt}',
+    },
+    bio: '${bio}',
+  }`;
+
+//
+export async function updateProfile(username, { avatar, banner }) {
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        'X-Noroff-API-Key': `${localStorage.getItem('apiKey')}`,
+      },
+      body: JSON.stringify(body),
+    });
+    if (!response.ok) {
+      const errors = await response.json();
+      alert(errors.errors[0].message);
+    }
+    const res = await response.json();
+    console.log(res);
+  } catch (error) {
+    console.error('Error:', responseData.errors[0].message);
+  }
+}
+
+// PUT
+// /social/profiles/{name}/posts
+const apiExample = {
+  avatar: {
+    url: 'string',
+    alt: '',
+  },
+  banner: {
+    url: 'string',
+    alt: '',
+  },
+  bio: 'string',
+};
