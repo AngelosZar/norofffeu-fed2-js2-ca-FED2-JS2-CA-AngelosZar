6375b75a43fb55491bb484b727d56a56eb0c1395
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-04 16:31:37 +0100

    card styles .additional fark mode

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -14,16 +14,41 @@ const renderMultiplePosts = async function (limit, page, tag) {
 
     const parentContainer = document.querySelector('#post-feed');
     responseData.forEach((post) => {
-      const html = `
-            <div class="card-for-posts">
-              <h3 class="title-for-post">${post.title}</h3>
-              <p class="body-for-post">${post?.body}</p>
-              <p class="tags-for-post">${post?.tags.join(' / ')}</p>
-              <img class="media-for-post" src="${
-                post?.media?.url ?? ''
-              }" alt="${post?.media?.alt ?? ''}" />
-              `;
+      // const html = `
+      //       <div class="card-for-posts">
+      //         <h3 class="title-for-post">${post.title}</h3>
+      //         <p class="body-for-post">${post?.body}</p>
+      //         <p class="tags-for-post">${post?.tags.join(' / ')}</p>
+      //         <img class="media-for-post" src="${
+      //           post?.media?.url ?? ''
+      //         }" alt="${post?.media?.alt ?? ''}" />
+      //         `;
 
+      // parentContainer.insertAdjacentHTML('beforeend', html);
+      const html = `
+      <div class="container mx-auto max-w-[95%] md:max-w-[85%]" id="post-feed">
+        <div class="card-for-posts mb-8 rounded-md border-2 border-slate-300 bg-slate-50 text-gray-600 dark:bg-gray-700 dark:text-white">
+          <h3 class="title-for-post px-4 py-2 text-xl font-bold">${post.title}</h3>
+          <p class="body-for-post px-4 pb-2 dark:text-gray-400">${post?.body}</p>
+          
+          <div class="flex max-h-[36rem] max-w-[62rem] items-center justify-center overflow-hidden">
+            <img
+              class="media-for-post h-auto w-full object-contain object-center"
+              src="${post?.media?.url ?? ''}"
+              alt="${post?.media?.alt ?? ''}"
+            />
+          </div>
+          
+          <p class="tags-for-post py-4 pl-4">${post?.tags.join(' / ')}</p>
+          
+          <div class="flex justify-around pb-4 [&>a]:rounded-md [&>a]:px-4 [&>a]:py-2 [&>a]:transition-colors">
+            <a href="#" id="like-btn" class="hover:bg-slate-300">Like 👍</a>
+            <a href="#" id="comment-btn" class="hover:bg-slate-300">Comment 💬</a>
+            <a href="#" id="share-btn" class="hover:bg-slate-300">Share ↪</a>
+          </div>
+        </div>
+      </div>
+      `;
       parentContainer.insertAdjacentHTML('beforeend', html);
       const postId = post.id;
       return postId;
@@ -36,4 +61,4 @@ const renderMultiplePosts = async function (limit, page, tag) {
 };
 
 document.addEventListener('DOMcontentLoaded', checkForCredentials());
-// await renderMultiplePosts(12, 1, 'tag');
+await renderMultiplePosts(12, 1, 'tag');
