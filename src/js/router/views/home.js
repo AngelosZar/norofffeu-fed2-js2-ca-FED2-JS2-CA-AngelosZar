236a06e2db698729b0c114df6bba647f0998994e
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-10 20:32:22 +0100

    fixed styles on the main post card

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -21,44 +21,44 @@ const renderMultiplePosts = async function () {
     responseData.forEach(async (post) => {
       const { name, email, bio, avatar, banner, avatarUrl, bannerUrl, avatarAlt } =
         extractAuthorInfo(post);
-
       const html = `
-      <div class="container mx-auto max-w-[95%] md:max-w-[85%]" id="post-feed">
-        <div class="card-for-posts mb-8 rounded-md border-2 border-slate-300 bg-slate-50 text-gray-600 dark:bg-gray-700 dark:text-white" data-post-id="${post.id}">
-          <div class="grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
-            <div class="flex w-full items-center gap-4 pl-4 md:pl-6">
-              <img
-                src="${avatarUrl}"
-                class="flex h-16 w-16 flex-shrink-0 rounded-full object-cover"
-            alt="${avatarAlt}"
-              />
-              <div class="flex flex-col">
-                <p id="userAvatarOnPost" class="">${name}</p>
-                <p id="timeOfPost" class="flex max-w-[80%] dark:text-gray-400">${formatPostDate(post.created)}</p>
+        <div class="container mx-auto max-w-[95%] md:max-w-[85%]" id="post-feed">
+          <div class="card-for-posts mb-8 rounded-md border-2 border-slate-300 bg-slate-50 text-gray-600 dark:bg-gray-700 dark:text-white" data-post-id="${post.id}">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-[1fr_3fr]">
+              <div class="flex w-full items-center gap-4 p-4 md:pl-6 ">
+                <img
+                  src="${avatarUrl}"
+                  class="flex h-16 w-16 flex-shrink-0 rounded-full object-cover"
+                  alt="${avatarAlt}"
+                />
+                <div class="flex flex-col">
+                  <p id="userAvatarOnPost" class="">${name}</p>
+                  <p id="timeOfPost" class="flex max-w-[80%] dark:text-gray-400">${formatPostDate(post.created)}</p>
+                </div>
               </div>
+         <div class="flex w-full flex-shrink flex-col md:pl-8 md:pt-2">
+         <h3 class="title-for-post px-4 py-2 text-xl font-bold truncate max-w- hover:text-clip hover:whitespace-normal text-right">${post.title}</h3>
+          <p class="body-for-post px-4 pb-2 dark:text-gray-400 text-right">${post?.body}</p>
+</div>
             </div>
-            <div class="flex w-full flex-shrink flex-col md:pl-2 md:pt-2">
-              <h3 class="title-for-post px-4 py-2 text-xl font-bold">${post.title}</h3>
-              <p class="body-for-post px-4 pb-2 dark:text-gray-400">${post?.body}</p>
+            <div class="flex max-h-[36rem] max-w-[62rem] items-center justify-center overflow-hidden">
+              <img
+                class="media-for-post h-auto w-full object-contain object-center"
+                src="${post?.media?.url ?? ''}"
+                alt="${post?.media?.alt ?? ''}"
+                data-post-id="${post.id}"
+              />
             </div>
-          </div>
-          <div class="flex max-h-[36rem] max-w-[62rem] items-center justify-center overflow-hidden">
-            <img
-              class="media-for-post h-auto w-full object-contain object-center"
-              src="${post?.media?.url ?? ''}"
-              alt="${post?.media?.alt ?? ''}"
-              data-post-id="${post.id}"
-            />
-          </div>
-          <p class="tags-for-post py-4 pl-4">${post?.tags.join(' / ')}</p>
-          <div class="flex justify-around pb-4 [&>a]:rounded-md [&>a]:px-4 [&>a]:py-2 [&>a]:transition-colors flex-wrap">
-            <a href="#" id="like-btn" class="hover:bg-slate-300">Like 👍</a>
-            <a href="#" id="comment-btn" class="hover:bg-slate-300">Comment 💬</a>
-            <a href="#" id="share-btn" class="hover:bg-slate-300">Share ↪</a>
+            <p class="tags-for-post py-4 pl-4">${post?.tags.join(' / ')}</p>
+            <div class="flex justify-around pb-4 [&>a]:rounded-md [&>a]:px-4 [&>a]:py-2 [&>a]:transition-colors flex-wrap">
+              <a href="#" id="like-btn" class="hover:bg-slate-300">Like 👍</a>
+              <a href="#" id="comment-btn" class="hover:bg-slate-300">Comment 💬</a>
+              <a href="#" id="share-btn" class="hover:bg-slate-300">Share ↪</a>
+            </div>
           </div>
         </div>
-      </div>
       `;
+
       parentContainer.insertAdjacentHTML('beforeend', html);
       const postId = post.id;
       return postId;
@@ -76,3 +76,35 @@ await renderMultiplePosts();
 // await readPosts(12, 1, 'tag');
 
 //
+// </div>
+//               <img
+//                 src="${avatarUrl}"
+//                 class="flex h-16 w-16 flex-shrink-0 rounded-full object-cover"
+//             alt="${avatarAlt}"
+//               />
+//               <div class="flex flex-col">
+//                 <p id="userAvatarOnPost" class="">${name}</p>
+//                 <p id="timeOfPost" class="flex max-w-[80%] dark:text-gray-400">${formatPostDate(post.created)}</p>
+//               </div>
+//             </div>
+//             <div class="flex w-full flex-shrink flex-col md:pl-2 md:pt-2">
+//               <h3 class="title-for-post px-4 py-2 text-xl font-bold">${post.title}</h3>
+//               <p class="body-for-post px-4 pb-2 dark:text-gray-400">${post?.body}</p>
+//             </div>
+//           </div>
+//           <div class="flex max-h-[36rem] max-w-[62rem] items-center justify-center overflow-hidden">
+//             <img
+//               class="media-for-post h-auto w-full object-contain object-center"
+//               src="${post?.media?.url ?? ''}"
+//               alt="${post?.media?.alt ?? ''}"
+//               data-post-id="${post.id}"
+//             />
+//           </div>
+//           <p class="tags-for-post py-4 pl-4">${post?.tags.join(' / ')}</p>
+//           <div class="flex justify-around pb-4 [&>a]:rounded-md [&>a]:px-4 [&>a]:py-2 [&>a]:transition-colors flex-wrap">
+//             <a href="#" id="like-btn" class="hover:bg-slate-300">Like 👍</a>
+//             <a href="#" id="comment-btn" class="hover:bg-slate-300">Comment 💬</a>
+//             <a href="#" id="share-btn" class="hover:bg-slate-300">Share ↪</a>
+//           </div>
+//         </div>
+//       </div>
